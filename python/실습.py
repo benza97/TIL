fe37a75dab4f516d9d32560681fd36625e013b8f
benza97 <benza97@naver.com>
2023-01-02 17:37:26 +0900

    python

---
+++ python/실습.py
@@ -0,0 +1,76 @@
+number = int(input('숫자를 입력해주세요 >'))
+print(number + 10)
+
+food = str(input('좋아하는 음식을 입력해주세요 >'))
+print(food)
+
+name = str(input('이름을 입력해주세요 >'))
+age = int(input('태어난 년도를 입력해주세요 >'))
+
+print('저의 이름은', (name), '이고')
+print('올해 나이는', (2024 - (age) , '입니다.'))
+
+
+a = str(input('첫 번째 문장을 입력해주세요 >'))
+b = str(input('두 번째 문장을 입혁해주세요 >'))
+
+print(a+b)
+
+c = int(input('숫자를 입력해 주세요 >'))
+print(-c)
+
+d = int(input('첫 번쨰 숫자를 입력해주세요 >'))
+e = int(input('두 번째 숫자를 입력해주세요 >'))
+
+print('+13', d+13) 
+print('+13', e+13)
+print('-7', d-7)
+print('-7', e-7)
+print('*30', d*30)
+print('*30', e*30)
+print('/3', d/3)
+print('/3', e/3)
+print('%1', d%1)
+print('%1', e%1)
+
+
+z = int(input('첫 번째 숫자를 입력해주세요 >'))
+x = int(input('두 번째 숫자를 입력해주세요 >'))
+c = int(input('세 번째 숫자를 입력해주세요 >'))
+
+list = [z, x, c]
+mean = sum(list) / len(list)
+print(mean)
+
+
+
+
+
+
+
+
+ 
+
+v = int(input('첫 번째 숫자를 입력해주세요 >'))
+b = int(input('두 번째 숫자를 입력해주세요 >'))
+n = int(input('세 번째 숫자를 입력해주세요 >'))
+m = int(input('네 번째 숫자를 입력해주세요 >'))
+l = int(input('다섯 번째 숫자를 입력해주세요 >'))
+
+print([v,b,n,m,l])
+
+q = str(input('문자열을 입력해주세요 >'))
+w = int(input('숫자를 입력해 주세요'))
+
+print(q*w)
+
+e = int(input('첫 번째 숫자를 입력해주세요 >'))
+r = int(input('두 번째 숫자를 입력해주세요 >'))
+t = int(input('세 번째 숫자를 입력해주세요 >'))
+y = int(input('네 번째 숫자를 입력해주세요 >'))
+u = int(input('다섯 번째 숫자를 입력해주세요 >'))
+
+print(e+r+t+y+u) # 잘 모르겠음
+
+
+
